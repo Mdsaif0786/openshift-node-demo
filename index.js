a01337451b15ea53b9147c0496754b7a5bb5964f
Mdsaif0786 <mdsaifrza11@gmail.com>
2025-12-09 18:26:21 +0530

    Added port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 
 
-
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello World");
@@ -12,6 +12,6 @@ app.get("/", (req, res) => {
 app.get("/health", (req, res) => {
     res.status(200).send({statusCode: 200, message: "Welcome to the openshift using Node.js"});
 });
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is running on port 3000");
 });
