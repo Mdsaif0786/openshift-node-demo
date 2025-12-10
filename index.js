c41fa802ff659fab4341a8a9fd165e7b4305e136
Mdsaif0786 <mdsaifrza11@gmail.com>
2025-12-10 16:48:38 +0530

    Added child onboarding API

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log("Hello World");
 import express from "express";
 const app = express();
 
+app.use(express.json());
 
 
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,14 @@ app.get("/", (req, res) => {
 app.get("/health", (req, res) => {
     res.status(200).send({statusCode: 200, message: "Welcome to the openshift using Node.js"});
 });
+app.post('/child-onboarding', (req, res)=>{
+    const {name, age, gender} = req.body;
+    console.log('REceived body', req.body);
+    if(!name || !age || !gender){
+        return res.status(400).send({statusCode: 400, message: "Name, age and gender are required"});
+    }
+    res.status(200).send({statusCode: 200, message: "Child onboarded successfully"});
+})
 app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
